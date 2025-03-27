b9c24d81518f410d07a53c89ea31e94219cfa03c
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-03-27 13:37:06 +0530

    image adding functionality

--- src/Components/AddProject.jsx
+++ src/Components/AddProject.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 
@@ -8,17 +8,72 @@ import uploadimage from "../assets/uploadIMG.jpg";
 const AddProject = () => {
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    clearContent();
+    setShow(false);
+  };
   const handleShow = () => setShow(true);
-  const [projectData,setProjectData]=useState({
-    projectImg:"",
-    projectTitle:"",
-    projectLanguage:"",
-    projectOverview:"",
-    projectGitLink:"",
-    projectWebsiteLink:""
-  })
+  const [preview, setPreview] = useState("");
+  const [imgTypeStatus, setImgTypeStatus] = useState(false);
+  const [projectData, setProjectData] = useState({
+    projectImg: "",
+    projectTitle: "",
+    projectLanguage: "",
+    projectOverview: "",
+    projectGitLink: "",
+    projectWebsiteLink: "",
+  });
 
+
+  
+  const onBtnClick = () => {
+    if (
+      projectData.projectGitLink &&
+      projectData.projectImg &&
+      projectData.projectLanguage &&
+      projectData.projectOverview &&
+      projectData.projectTitle &&
+      projectData.projectWebsiteLink
+    ) {const payload=new FormData()
+
+      payload.append("projectImg",projectData.projectImg)
+      payload.append("projectTitle",projectData.projectTitle)
+      payload.append("projectLanguage",projectData.projectLanguage)
+      payload.append("projectOverview",projectData.projectOverview)
+      payload.append("projectGitLink",projectData.projectGitLink)
+      payload.append("projectWebsiteLink",projectData.projectWebsiteLink)
+    }else{'please fill the form correctly'}
+  };
+  //showing image
+  useEffect(() => {
+    if (projectData.projectImg) {
+      if (
+        projectData.projectImg.type == "image/png" ||
+        projectData.projectImg.type == "image/jpg" ||
+        projectData.projectImg.type == "image/jpeg"
+      ) {
+        setImgTypeStatus(true);
+        setPreview(URL.createObjectURL(projectData.projectImg));
+      } else {
+        setImgTypeStatus(false);
+        alert("please upload only specified data");
+      }
+    }
+  }, [projectData.projectImg]);
+
+  //used to clear contents inside the modal
+  const clearContent = () => {
+    setProjectData({
+      ...projectData,
+      projectImg: "",
+      projectTitle: "",
+      projectLanguage: "",
+      projectOverview: "",
+      projectGitLink: "",
+      projectWebsiteLink: "",
+    });
+    setPreview(""), setImgTypeStatus(false);
+  };
 
   return (
     <>
@@ -38,35 +93,82 @@ const AddProject = () => {
           <div className="row">
             <div className="col-lg-4">
               <label>
-                <input type="file" style={{ display: "none" }} />
-                <img className="img-fluid" src={uploadimage} alt="" />
+                <input
+                  onChange={(e) =>
+                    setProjectData({
+                      ...projectData,
+                      projectImg: e.target.files[0],
+                    })
+                  }
+                  type="file"
+                  style={{ display: "none" }}
+                />
+                <img
+                  className="img-fluid"
+                  src={preview ? preview : uploadimage}
+                  alt=""
+                />
               </label>
-              <p className="text-warning fw-bolder mt-3">
-                *upload only the following file types (jpeg,jpg,png) here!!!*
-              </p>
+              {!imgTypeStatus ? (
+                <p className="text-warning fw-bolder mt-3">
+                  *upload only the following file types (jpeg,jpg,png) here!!!*
+                </p>
+              ) : (
+                ""
+              )}
             </div>
             <div className="col-lg-8">
-              <input onChange={(e)=>setProjectData({...projectData,projectTitle:e.target.value})}
+              <input
+                onChange={(e) =>
+                  setProjectData({
+                    ...projectData,
+                    projectTitle: e.target.value,
+                  })
+                }
                 type="text"
                 placeholder="project Title"
                 className="form-control mt-2"
               />
-              <input onChange={(e)=>setProjectData({...projectData,projectLanguage:e.target.value})}
+              <input
+                onChange={(e) =>
+                  setProjectData({
+                    ...projectData,
+                    projectLanguage: e.target.value,
+                  })
+                }
                 type="text"
                 placeholder="project Language"
                 className="form-control mt-2"
               />
-              <input onChange={(e)=>setProjectData({...projectData,projectOverview:e.target.value})}
+              <input
+                onChange={(e) =>
+                  setProjectData({
+                    ...projectData,
+                    projectOverview: e.target.value,
+                  })
+                }
                 type="text"
                 placeholder="project OverView"
                 className="form-control mt-2"
               />
-              <input onChange={(e)=>setProjectData({...projectData,projectGitLink:e.target.value})}
+              <input
+                onChange={(e) =>
+                  setProjectData({
+                    ...projectData,
+                    projectGitLink: e.target.value,
+                  })
+                }
                 type="text"
                 placeholder="project GitHub Link"
                 className="form-control mt-2"
               />
-              <input onChange={(e)=>setProjectData({...projectData,projectWebsiteLink:e.target.value})}
+              <input
+                onChange={(e) =>
+                  setProjectData({
+                    ...projectData,
+                    projectWebsiteLink: e.target.value,
+                  })
+                }
                 type="text"
                 placeholder="project Website Link"
                 className="form-control mt-2"
@@ -78,7 +180,9 @@ const AddProject = () => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button onClick={onBtnClick} variant="primary">
+            Understood
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
